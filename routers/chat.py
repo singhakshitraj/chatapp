2b137633e1c33079769d82c95d1c315ffcd85a2c
Akshit Raj Singh <akshitraj.rkt@gmail.com>
2025-10-11 11:52:38 +0530

    feat: add group chat functionality
    - Users can now add other participants to existing chats
    - Participants are stored in Redis for faster access and to maintain active connections
    - Underlying implementation supports multiple users per chat, also including persistent storage for offline participants
    - fix: prevented users from being added multiple times or adding themselves repeatedly

--- routers/chat.py
+++ routers/chat.py
@@ -4,6 +4,7 @@ from db.connection_db import db_connect
 from db.connection_redis import get_redis
 from utils.token import JWTTokenClass
 from psycopg2.errors import ReadingSqlDataNotPermitted,DatabaseError
+from utils.validation_models import add_contact_validation
 router=APIRouter(
     prefix= '/chat/{chat_id}'
 )
@@ -18,8 +19,26 @@ def get_chat_participants(chat_id:str,redis=Depends(get_redis),username=Depends(
     except ReadingSqlDataNotPermitted as e:
         return JSONResponse({'message':'You are not a participant in this chat.','remarks':'Permission Denied,'},status_code=status.HTTP_401_UNAUTHORIZED)
 
+@router.post('/participants/add')
+def add_participant(chat_id:str,contact:add_contact_validation,username=Depends(JWTTokenClass.get_user),redis=Depends(get_redis),connection=Depends(db_connect)):
+    participants=redis.lrange(chat_id,0,-1)
+    if username not in participants:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail={'message':'Not authorized to add people!'})
+    if contact.username in participants:
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE,detail={'message':'User already part of group!!'})
+    with connection.cursor() as cursor:
+        try:
+            cursor.execute('INSERT INTO user_chats(username,chat_id) VALUES(%s,%s) RETURNING *',(contact.username,chat_id))
+            result=cursor.fetchone()
+            if result is None:
+                raise DatabaseError
+            redis.lpush(chat_id,contact.username)
+            return JSONResponse(status_code=status.HTTP_201_CREATED,content={'message':'Participant Added!'})
+        except DatabaseError as e:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail={'message':'Unable to add participant'})
+
 @router.get('/messages')
-def get_messages(chat_id:str,limit=Query(20),offset=Query(20),username=Depends(JWTTokenClass.get_user),connection=Depends(db_connect),redis=Depends(get_redis)):
+def get_messages(chat_id:str,limit=Query(20),offset=Query(0),username=Depends(JWTTokenClass.get_user),connection=Depends(db_connect),redis=Depends(get_redis)):
     participants=redis.lrange(chat_id,0,-1)
     if username not in participants:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail={'message':'You are not a participant in this chat.'})
@@ -33,6 +52,14 @@ def get_messages(chat_id:str,limit=Query(20),offset=Query(20),username=Depends(J
             ''',(chat_id,limit,offset)
             )
             messages=cursor.fetchall()
-            return JSONResponse(content={'data':messages},status_code=status.HTTP_200_OK)
+            all_messages=[{
+                'message_id':message.get('message_id'),
+                'datetime':str(message.get('date_time')),
+                'message':message.get('message'),
+                'sent_by':message.get('sent_by')
+            } for message in messages]
+            return JSONResponse(
+                content=all_messages,
+                status_code=status.HTTP_200_OK)
         except DatabaseError as e:
             return JSONResponse(content={'error':'Unable to fetch data'},status_code=status.HTTP_503_SERVICE_UNAVAILABLE)
